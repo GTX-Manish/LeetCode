2b49a9b16ae45252f2e76903c221eeb7e2a50d6a
Manish Kumar <footballstarmanish@gmail.com>
2024-10-18 06:12:24 +0530

    Update 1. Two Sum.cpp

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mp;
+        unordered_map<int, int> mp; // creating an unordered_map
 
         for(int i = 0; i < nums.size(); i++){
-            int comp = target - nums[i];
+            int comp = target - nums[i]; 
+            // to check if comp is map or not
             if(mp.count(comp)){
-                return {mp[comp], i};
+                return {mp[comp], i}; // return if found
             }
-            
             mp[nums[i]] = i;
         }
-        return {-1,-1};
+        return {-1,-1}; // return {-1,-1} if answer not found
     }
 };
